74c0d90171a2178c4f29ceacab5ef061af8e8386
AlexDelei <alexdelei125@gmail.com>
2024-01-03 12:18:43 -0800

    Ruby ruby

---
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+# Checking if argument exists
+if ARGV.empty?
+  puts "Usage: ruby match_patterns.rb <input>"
+else
+  # Obtaining the argument
+  input = ARGV[0]
+
+  #definng the pattern
+  pattern =  /^h(.{1,})n$/
+
+  matches = input.scan(pattern).flatten
+
+  if matches.any?
+    puts input
+  else
+    puts ""
+  end
+end
