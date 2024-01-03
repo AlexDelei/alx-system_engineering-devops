17243293836ee0f16c3bca618eaf2f60d08ed33f
AlexDelei <alexdelei125@gmail.com>
2024-01-03 11:59:41 -0800

    Ruby ruby

---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+#
+# Checks for arguments
+if ARGV.empty?
+  puts "Usage: ruby match_patterns.rb <input>"
+else
+  # Obtain the argument
+  input = ARGV[0]
+
+  # Define the pattern
+  pattern = /^(?!hbon$).*$/
+
+  matches = input.scan(pattern).flatten
+
+  if matches.any?
+    puts input
+  else
+    puts ""
+  end
+end
