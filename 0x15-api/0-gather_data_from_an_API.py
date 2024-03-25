3e32a2f4537ac7b2b84c50877465a39993ddadfe
AlexDelei <alexdelei125@gmail.com>
2024-03-25 06:29:23 -0700

    provide emplyee id then list his or her name, the number of tasks done and title of tasks done

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# API call and display of data
+import requests
+import sys
+
+
+def getUser(userId):
+    """print out how many todos a certain user has done"""
+    r = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos/')
+
+    tasks_done = tasks.json()
+    done_list = []
+    not_done = []
+    task_title = []
+    for i in tasks_done:
+        if i['userId'] == int(userId):
+            if i['completed'] == True:
+                done_list.append(i['completed'])
+            if i['completed'] == False:
+                not_done.append(i['completed'])
+            if i['completed'] == True:
+                task_title.append(i['title'])
+
+    data = r.json()
+    name = data.get('name')
+    done = len(done_list)
+    Notdone = len(not_done)
+    print("Employee {} is done with tasks {}/{}:".format(name, done, Notdone))
+    for i in task_title:
+        print("\t{}".format(i))
+
+
+if __name__ == '__main__':
+    user_id = sys.argv[1]
+    getUser(user_id)
