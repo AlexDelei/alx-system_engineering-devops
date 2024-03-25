8f34ba63417fe99a85118fc58318a18ed73a853d
AlexDelei <alexdelei125@gmail.com>
2024-03-25 10:32:27 -0700

    extended to convert to pdf

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,28 +1,50 @@
 #!/usr/bin/python3
-"""Exporting data into a csv file"""
+""" API call and display of data"""
 import csv
 import requests
 import sys
 
 
-def export_csv(userId):
-    """Use the provided userId as the name of the file"""
-    r = requests.get('https://jsonplaceholder.typicode.com/todos/')
-    user = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+def export_csv(userId, data, username):
+    """Export data to CSV file"""
+    csv_file = f"{userId}.csv"
+    with open(csv_file, mode="w", newline='') as f:
+        w = csv.writer(f)
+        w.writerow(['USER_ID',
+                    'USERNAME',
+                    'TASK_COMPLETED_STATUS',
+                    'TASK_TITLE'])
+        for task in data:
+            w.writerow([userId, username, task['completed'], task['title']])
+
+
+def getUser(userId):
+    """print out how many todos a certain user has done"""
+    r = requests.get(f"https://jsonplaceholder.typicode.com/users/{userId}")
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos/')
+
+    tasks_done = tasks.json()
+    done_list = []
+    not_done = []
+    task_title = []
+    for i in tasks_done:
+        if i['userId'] == int(userId):
+            if i['completed']:
+                done_list.append(i['completed'])
+                task_title.append(i['title'])
+            else:
+                not_done.append(i['completed'])
 
     data = r.json()
-    name = user.json().get('name')
-    csv_file = f"{userId}.csv"
-
-    with open(csv_file, mode="w", newline='') as f:
-        wr = csv.writer(f)
-        wr.writerow(['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE'])
-
-        for j in data:
-            if j['userId'] == int(userId):
-                wr.writerow([j['userId'], name, j['completed'], j['title']])
+    name = data.get('name')
+    export_csv(userId, tasks_done, name)
+    done = len(done_list)
+    total = len(not_done) + done
+    print("Employee {} is done with tasks({}/{}):".format(name, done, total))
+    for i in task_title:
+        print("\t {}".format(i))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     user_id = sys.argv[1]
-    export_csv(user_id)
+    getUser(user_id)
