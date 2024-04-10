fa49e248d9531dc4ba281c69ede0e51392d5463d
AlexDelei <alexdelei125@gmail.com>
2024-04-10 05:20:55 +0000

    number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Reddit API
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """
+    return the number of subscribers
+    """
+    url = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
+    headers = {
+        "User-Agent": "linux:0x16.api.advance:v1.0.0 (by /u/bdov)"
+    }
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 400:
+        return 0
+    data = r.json().get("data")
+    return data.get("subscribers")
