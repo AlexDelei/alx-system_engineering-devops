b59a671daf52290c2909867088adaca0dcb5b8e1
AlexDelei <alexdelei125@gmail.com>
2024-04-11 12:57:27 -0700

    all subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Get the no of subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'My-User-Agent'}
     try:
+        headers = {'User-Agent': 'My-User-Agent'}
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
         respo = requests.get(url, headers=headers, allow_redirects=False)
         data = respo.json()
         return data.get('data').get('subscribers')
