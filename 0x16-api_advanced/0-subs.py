7e6eb08a450224122703b9e3ae98d9c0e5b3e0db
AlexDelei <alexdelei125@gmail.com>
2024-04-11 12:50:16 -0700

    all subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,12 +8,12 @@ import requests
 def number_of_subscribers(subreddit):
     """Get the no of subscribers"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Custom User Agent'}
+    headers = {'User-Agent': 'My-User-Agent'}
     try:
         respo = requests.get(url, headers=headers, allow_redirects=False)
-        if respo.statuc_code == 200:
+        if respo.status_code == 200:
             data = respo.json()
-            return data['data']['subscribers']
+            return data.get('data').get('subscribers')
         else:
             return 0
     except Exception as e:
