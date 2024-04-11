4c3714ac88c13236cfeb1a34bf47b44f4160d06b
AlexDelei <alexdelei125@gmail.com>
2024-04-11 13:47:40 -0700

    top ten hot posts

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
     the top ten queries
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {'User-Agent': 'Custom User Agent'}
+    headers = {'User-Agent': 'My-User-Agent'}
     try:
         respo = requests.get(url, headers=headers, allow_redirects=False)
         if respo.status_code == 200:
