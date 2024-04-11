68539814cd3cdcc7616422448abc92c28490ac50
AlexDelei <alexdelei125@gmail.com>
2024-04-11 13:10:57 -0700

    List top ten hot posts

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Query the top ten hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    the top ten queries
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Custom User Agent'}
+    try:
+        respo = requests.get(url, headers=headers, allow_redirects=False)
+        if respo.status_code == 200:
+            data = respo.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
+       return None
+    except Exception:
+        return None
